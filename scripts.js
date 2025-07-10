ab17cc49f7ddcd4d33a22ed2ca0c70d3379473fd
hannes2rpi <shannert134340@gmail.com>
2025-07-10 01:29:53 -0500

    LOTS OF UPDATES
    -finalized 7/10/25
    -added content for hobbies, projects, and certifications
    -fixed resume
    -edited color scheme
    -fixed spacing

--- scripts.js
+++ scripts.js
@@ -6,21 +6,13 @@ function changeHobby() {
     let hobbySelection = document.getElementById("hobbies")
     hobbySelection.addEventListener("change", function(){
         if (hobbies.value == "cooking"){
-            var text = "Check out this wholesome vegan meal!";
+            var text = "<div class='hobbies_boxes'><div class='hobbies_boxes2'><img src='../img/macncheese.jpeg' width=250 height=350 alt='delicious mac n cheese'></div><div class='hobbies_boxes2'><img src='../img/tofu_marsala.jpeg' width=250 height=350 alt='tofu marsala including tofu, mushrooms, mashed potatoes, and green beans'></div><div class='hobbies_boxes2'><img src='../img/cheeseburger_salad.jpeg' width=250 height=350 alt='cheeseburger salad featuring a black bean burger, lettuce, and toppings'></div></div><div class='hobbies_boxes_smaller'><div class='hobbies_boxes2_smaller'><p>Mac n Cheese<br></br>Dairy free cheese, cavatappi noodles, and hearts of palm baked with old bay seasoning. Topped with breadcrumbs and tarragon.</p></div><div class='hobbies_boxes2_smaller'><p>Tofu Marsala<br></br>Tofu, mushrooms, mashed potatoes, and green beans covered in a marsala sauce.</p></div><div class='hobbies_boxes2_smaller'><p>Cheeseburger Salad<br></br>Black bean burger served over lettuce with dairy free cheese, purple onion, tomatoes, pickles, sesame seeds, and secret sauce.</p></div></div>";
             document.getElementById("response").innerHTML = text;
             console.log("Changed to cooking");
-        }else if (hobbies.value == "minecraft"){
-            var text = "here is my awesome minecraft roller coaster!";
+        }else if (hobbies.value == "more"){
+            var text = "More coming soon! Should I add gaming, crocheting, or web design next? OH I could've just screenshoted this website. Check out my website... on my website! Hmm maybe I should add comedy instead lol.";
             document.getElementById("response").innerHTML = text;
-            console.log("Changed to minecraft");
-        }else if (hobbies.value == "league"){
-            var text = "Check out this amazing Seraphine ult";
-            document.getElementById("response").innerHTML = text;
-            console.log("Changed to league");
-        }else if (hobbies.value == "rivals"){
-            var text = "Check out this amazing Namor ult";
-            document.getElementById("response").innerHTML = text;
-            console.log("Changed to rivals");
+            console.log("Changed to more");
         }
     })  
 }
